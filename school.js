3e2b81b77796fd6fc407571b454b7cf02f9032db
Gregory Beaver <greg@chiaraquartet.net>
2013-01-26 15:21:15 -0600

    new school extension, displays next date of school

---
+++ school.js
@@ -0,0 +1,38 @@
+(function(){
+ document.getElementsByClassName("{sorter: 'numeric'} header sortasc")[0].firstElementChild.innerHTML = 'Next juv.';
+ var schools = document.body.innerHTML.match(/escuelas_paises\.php\?[^"]+/g);
+ for (var i = 0; i < schools.length; i++) {
+  var school = schools[i];
+  var data = school.match(/id=(\d+)&amp;idpais=(\d+)/);
+  if (!data) continue;
+  (function() {
+   var idpais = data[2];
+   $.ajax({
+    url: school.replace('&amp;', '&'),
+    success: function(response) {
+     // find the data:
+     var doc = document.implementation.createHTMLDocument("stuff");
+     var date;
+     doc.documentElement.innerHTML = response;
+     var tr = doc.getElementsByTagName('tbody');
+     for (var j = 0; j < tr.length; j++) {
+      if (tr[j].className == 'pijama') {
+       continue;
+      }
+      date = tr[j].lastElementChild.lastElementChild.innerHTML;
+      break;
+     }
+     // find the place to put the data:
+     var tr = document.getElementsByTagName('tr');
+     for (var j = 0; j < tr.length; j++) {
+      if (tr[j].attributes['id_pais'] && tr[j].attributes['id_pais'].nodeValue == idpais) {
+       var container = tr[j].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling;
+       container.innerHTML = date;
+       break;
+      }
+     }
+    }
+   });
+  })();
+ }
+})();
